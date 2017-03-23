017631f2af1eff303480bd634b4c533286cb0922
Noah Berman <noah@noahberman.org>
2017-03-23 10:52:51 -0600

    add optionals lesson

---
+++ optionals.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var number: Int?
+
+print (number)
+
+let userEnteredText = "three"
+
+let userEnteredInteger = Int(userEnteredText)
+
+if let catAge = userEnteredInteger {
+ print (catAge * 7)
+} else {
+    // show an error message to the user
+}
